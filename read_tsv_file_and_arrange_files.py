526e4c44c12bac22e6b5d12b552142828403b4b4
lorewar2 <mw8511@live.com>
2024-10-30 12:37:24 -0500

    update putting file in folders

--- read_tsv_file_and_arrange_files.py
+++ read_tsv_file_and_arrange_files.py
@@ -14,7 +14,19 @@ print(df.columns)
 print(df[["donor_organism.biomaterial_core.biomaterial_id", "donor_organism.sex", "file_format", "file_name"]])
 
 for index, row in df.iterrows():
-    print(row['donor_organism.biomaterial_core.biomaterial_id'], row['file_name'])
-    newpath = row['donor_organism.biomaterial_core.biomaterial_id'] 
-    if not os.path.exists(newpath):
-        os.makedirs(newpath)
+    #print(row['donor_organism.biomaterial_core.biomaterial_id'], row['file_name'])
+    newpath = row['donor_organism.biomaterial_core.biomaterial_id']
+    file_name = row['file_name']
+    if type(newpath) == str:
+        if len(newpath) > 5:
+            #print(newpath)
+            newpath = newpath[0:2]
+            newpath = newpath + "P"
+            #print(newpath)
+        source = "./" + file_name
+        destination = "./" + newpath + "/" + file_name
+        print(destination)
+        try:
+            os.rename(source, destination)
+        except:
+            print("what")
