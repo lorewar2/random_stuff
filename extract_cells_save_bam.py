d81b752e43cf65f0513882366aa509a6408adf67
lorewar2 <mw8511@live.com>
2024-11-13 17:24:41 -0600

    extract scipt done i guess

--- extract_cells_save_bam.py
+++ extract_cells_save_bam.py
@@ -1,6 +1,8 @@
-import pysam
 import random
+import os
 from collections import defaultdict
+import csv
+import pysam
 
 UNIQUE_CELLS = 10 # number of cells required
 BAR_CODE_MIN_READ = 10 # Min number of reads corrosponding to cell
@@ -11,21 +13,98 @@ SEED = 10
 
 def main():
     random.seed(SEED)
-    # Try for one bam first
-    bam_file_path = "/data1/cellector/kmeans_pp/split_by_doner/CB1/MantonCB1_HiSeq_4/outs/possorted_genome_bam.bam"
-    num_unique_cbs = UNIQUE_CELLS  # Number of unique CB tags to sample
-    sampled_reads = sample_bam_by_cb_tag(bam_file_path, num_unique_cbs)
-    # Print information about sampled reads
-    for cb_tag, reads in sampled_reads.items():
-        print(f"CB Tag: {cb_tag} {len(reads)}")
-    # Modify the reads
-    modified_reads = modify_cb_tags(sampled_reads)
-    # save the modified
-    save_modified_reads(modified_reads, OUTPUT_BAM_PATH, OUTPUT_BARCODES_PATH, bam_file_path)
-        #for read in reads:
-            #print(read)
+    experiment_bams_selected = select_the_bam_and_barcodes_to_process()
+    combined_reads = defaultdict(list)
+    # go through the bams and process them and what not
+    for index, experiment_bam_path in enumerate(experiment_bams_selected):
+        print("Processing -{} : {}".format(index, experiment_bam_path))
+        sampled_reads = sample_bam_by_cb_tag(experiment_bam_path, UNIQUE_CELLS)
+        modified_reads = modify_cb_tags(sampled_reads, "-{}".format(index))
+        # put in combined reads
+        # Iterate over each dictionary provided as input
+        for key, value in modified_reads.items():
+            # Append the value(s) for each key in the combined dictionary
+            # If the value is a list (e.g., reads associated with a CB tag), extend it
+            if isinstance(value, list):
+                combined_reads[key].extend(value)
+            else:
+                combined_reads[key].append(value)
+    save_modified_reads(combined_reads, OUTPUT_BAM_PATH, OUTPUT_BARCODES_PATH, experiment_bams_selected[0])
     return
 
+def modify_cb_tags(sampled_reads, modify_with):
+    # Dictionary to store the modified sampled reads with updated CB tags
+    modified_reads = {}
+    for cb_tag, reads in sampled_reads.items():
+        # Modify the CB tag from ending with "-1" to "-2"
+        if cb_tag.endswith("-1"):
+            new_cb_tag = cb_tag[:-2] + modify_with
+        else:
+            print(f"Warning: CB tag {cb_tag} does not end with '-1'. Skipping.")
+            continue
+        # Update the CB tag in each read
+        modified_reads[new_cb_tag] = []
+        for read in reads:
+            # Create a copy of the read to avoid modifying the original object (if needed)
+            read_copy = read
+            # Update the "CB" tag within the read to match the new CB tag
+            read_copy.set_tag("CB", new_cb_tag, value_type="Z")
+            # Add the modified read to the new CB tag list
+            modified_reads[new_cb_tag].append(read_copy)
+    return modified_reads
+
+def select_the_bam_and_barcodes_to_process():
+    experiments_selected = []
+    # Get  a list of doners (manual cause there are other folders :C)
+    donor_BL = ["BL{}".format(d) for d in range(1, 8)]
+    donor_CB = ["CB{}".format(d) for d in range(1, 12)]
+    donor_BM = ["BM{}".format(d) for d in range(1, 8)]
+    donors = [entry for entry in (donor_BL + donor_CB + donor_BM)]
+
+    # loop through the list and choose the one with highest umi/cell count
+    for donor_index, donor in enumerate(donors):
+        print("processing donor {} - {}".format(donor_index, donor))
+        # get the subdirectories
+        donor_experiments = list_subdirectories(donor)
+        umi_to_cell = [0] * len(donor_experiments)
+        # go through the experments and update umi list and select max
+        for experiment_index, experiment in enumerate(donor_experiments):
+            # get the umi to cell for this experiment
+            experiment_metric_csv_path = "./{}/{}/outs/metrics_summary.csv".format(donor, experiment)
+            try:
+                # Open the CSV file
+                #print(experiment_metric_csv_path)
+                with open(experiment_metric_csv_path, mode='r', newline='') as csvfile:
+                    # Read the CSV file
+                    reader = csv.reader(csvfile)
+                    # Skip the header row
+                    _ = next(reader)
+                    # Read the next row, which contains the values
+                    values = next(reader)
+                    # Get the last value in the row
+                    print(int(values[-1].replace(',','')))
+                    umi_to_cell[experiment_index] = int(values[-1].replace(',',''))
+            except:
+                print("0")
+        print(umi_to_cell)
+        max_index = umi_to_cell.index(max(umi_to_cell))
+        selected_experiment = donor_experiments[max_index]
+        print(max_index, selected_experiment)
+        experiments_selected.append("./{}/{}/outs/possorted_genome_bam.bam".format(donor, selected_experiment))
+    experiments_selected.sort()
+    print(experiments_selected)
+    return experiments_selected
+
+def list_subdirectories(directory_path):
+    try:
+        # List only directories within the specified path
+        subdirectories = [d for d in os.listdir(directory_path) if os.path.isdir(os.path.join(directory_path, d))]
+        return subdirectories
+    except FileNotFoundError:
+        return "The specified directory does not exist."
+    except PermissionError:
+        return "Permission denied for the specified directory."
+    
 def sample_bam_by_cb_tag(bam_file_path, num_unique_cbs):
     # Dictionary to store reads grouped by unique CB tags
     cb_dict = defaultdict(list)
@@ -66,29 +145,7 @@ def sample_bam_by_cb_tag(bam_file_path, num_unique_cbs):
             selected_cbs = random.sample(unique_cbs, num_unique_cbs)
         # Create a dictionary for the sampled reads by CB tag
         sampled_reads = {cb: cb_dict[cb] for cb in selected_cbs}
-
         return sampled_reads
-
-def modify_cb_tags(sampled_reads):
-    # Dictionary to store the modified sampled reads with updated CB tags
-    modified_reads = {}
-    for cb_tag, reads in sampled_reads.items():
-        # Modify the CB tag from ending with "-1" to "-2"
-        if cb_tag.endswith("-1"):
-            new_cb_tag = cb_tag[:-2] + "-2"
-        else:
-            print(f"Warning: CB tag {cb_tag} does not end with '-1'. Skipping.")
-            continue
-        # Update the CB tag in each read
-        modified_reads[new_cb_tag] = []
-        for read in reads:
-            # Create a copy of the read to avoid modifying the original object (if needed)
-            read_copy = read
-            # Update the "CB" tag within the read to match the new CB tag
-            read_copy.set_tag("CB", new_cb_tag, value_type="Z")
-            # Add the modified read to the new CB tag list
-            modified_reads[new_cb_tag].append(read_copy)
-    return modified_reads
 
 def save_modified_reads(modified_reads, output_bam_path, output_barcodes_path, template_path):
     # Get the list of unique CB tags
