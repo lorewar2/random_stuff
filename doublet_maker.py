b0d93998e9cee154f93764ecce665922abe18cc0
lorewar2 <mw8511@live.com>
2024-12-10 13:39:50 -0600

    update make doublet list

--- doublet_maker.py
+++ doublet_maker.py
@@ -7,27 +7,54 @@ import pysam
 SEED = 10
 INPUT_FILE_PATH = "1000_cell.bam"
 OUTPUT_PATH = "1000_with_doublets.bam"
+DOUBLET_PERCENT = 1
+
 def main():
     random.seed(SEED)
-    cb_dict, doublet_dict  = read_all_bam_files()
-    # combine pairs until doubet dict is empty
-    while len(doublet_dict) <= 1:
-        print(1)
-        # select 2
-        cells_to_remove = random.sample(list(doublet_dict.keys()), 2)
-        appended_reads = doublet_dict[cells_to_remove[0]] + doublet_dict[cells_to_remove[1]]
-        combined_key = "{}{}"
-        for cell in cells_to_remove:
-            del doublet_dict[cell]
-        # take 
-    for index, (key, value) in enumerate(doublet_dict):
-       print(index)
+    cb_dict, doublet_dics_by_donor  = read_all_bam_files()
+    doublet_list = make_a_doublet_list(doublet_dics_by_donor)
+    # Write to BAM the doublets
     return
+
+def make_a_doublet_list(doublet_dics_by_donor):
+    # Combine pairs until doubet dict is empty
+    doublet_dics_by_donor_empty = False
+    list_with_doublets = []
+    while doublet_dics_by_donor_empty == False:
+        # Select 2 donors and 1 cell from each
+        two_selected_donors = random.sample(range(len(doublet_dics_by_donor)), 2)
+        cell_to_remove_1 = random.sample(list(doublet_dics_by_donor[two_selected_donors[0]].keys()), 1)
+        cell_to_remove_2 = random.sample(list(doublet_dics_by_donor[two_selected_donors[1]].keys()), 1)
+        # Append the reads
+        appended_reads = doublet_dics_by_donor[two_selected_donors[0]][cell_to_remove_1] + doublet_dics_by_donor[two_selected_donors[1]][cell_to_remove_2]
+        cell_bar_code = random.choice([cell_to_remove_1, cell_to_remove_2])
+        combined_donors_string = "-{}-{}".format(two_selected_donors[0], two_selected_donors[1])
+        # change the cb tag on appended reads
+        for read in appended_reads:
+            # Create a copy of the read to avoid modifying the original object (if needed)
+            read_copy = read
+            # Update the "CB" tag within the read to match the new CB tag
+            new_cb_tag = cell_bar_code[:-2] + combined_donors_string
+            read_copy.set_tag("CB", new_cb_tag, value_type="Z")
+        list_with_doublets.append((combined_donors_string, cell_bar_code, appended_reads))
+        # Remove the cells from doublet_dics_by_donor
+        del doublet_dics_by_donor[two_selected_donors[0]][cell_to_remove_1]
+        del doublet_dics_by_donor[two_selected_donors[1]][cell_to_remove_2] 
+        if len(doublet_dics_by_donor[two_selected_donors[0]]) < 2:
+            del doublet_dics_by_donor[two_selected_donors[0]]
+        if len(doublet_dics_by_donor[two_selected_donors[1]]) < 2:
+            del doublet_dics_by_donor[two_selected_donors[1]]
+        # Update doublet_dics_by_donor_empty
+        current_number_of_cells = sum([len(dics) for dics in doublet_dics_by_donor])
+        if current_number_of_cells < 5 or len(doublet_dics_by_donor) < 3:
+            doublet_dics_by_donor_empty = True
+    return list_with_doublets
 
 def read_all_bam_files():
     combined_reads = defaultdict(list)
-    combined_doublets = defaultdict(list)
+    doublet_by_donor = []
     for index in range(27):
+        donor_doublets = defaultdict(list)
         bam_file_path = "{}{}".format(INPUT_FILE_PATH, index)
         cb_dict = defaultdict(list)
         with pysam.AlignmentFile(bam_file_path, "rb") as bam_file:
@@ -58,7 +85,8 @@ def read_all_bam_files():
             # Append the value(s) for each key in the combined dictionary
             # If the value is a list (e.g., reads associated with a CB tag), extend it
             if isinstance(value, list):
-                combined_doublets[key].extend(value)
+                donor_doublets[key].extend(value)
             else:
-                combined_doublets[key].append(value)
-    return (combined_reads, combined_doublets)
+                donor_doublets[key].append(value)
+        doublet_by_donor.append(donor_doublets)
+    return (combined_reads, doublet_by_donor)
