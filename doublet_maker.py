11ccec580f6ba4855d0881fa2ebd70583de5bf3f
minindu0 <mw8511@live.com>
2025-02-10 17:10:32 -0600

    update doublet maker

--- doublet_maker.py
+++ doublet_maker.py
@@ -6,21 +6,96 @@ import pysam
 
 SEED = 10
 INPUT_FILE_PATH = "merged.sorted.bam"
-OUTPUT_BAM_PATH = "350_with_doublets.bam"
-OUTPUT_BAR_PATH = "350_with_doublets.tsv"
+INPUT_BAR_PATH = "merged.sorted.tsv"
+OUTPUT_BAM_PATH = "test.bam"
+OUTPUT_BAR_PATH = "test.tsv"
 DOUBLET_PERCENT = 1
 
 def main():
     # Random initialize with seed
     random.seed(SEED)
+    read_count ()
+    doublet_list = open_bar_code_file_get_doublet_cells(INPUT_BAR_PATH)
     # Get all the reads by donor and doublets by donor
-    all_dics_by_donor, doublet_dics_by_donor  = read_all_bam_files()
+    all_dics_by_donor, doublet_dics_by_donor  = read_all_bam_files(doublet_list)
     print("doublet", len(doublet_dics_by_donor))
+    for index in range(51):
+        print("len ", index, len(doublet_dics_by_donor[50 - index]))
+        if len(doublet_dics_by_donor[50 - index]) == 0:
+            del doublet_dics_by_donor[50 - index]
     doublet_list = make_a_doublet_list(doublet_dics_by_donor)
-    print("final doublet list len ", len(doublet_list))
+    #print("final doublet list len ", len(doublet_list))
     # Write to BAM the doublets
     save_modified_reads(all_dics_by_donor, doublet_list, OUTPUT_BAM_PATH, OUTPUT_BAR_PATH, INPUT_FILE_PATH)
     return
+
+def read_count ():
+    bam_file_path = "{}".format(INPUT_FILE_PATH)
+    index = 0
+    with pysam.AlignmentFile(bam_file_path, "rb") as bam_file:
+        # Iterate through all reads in the BAM file
+        for read in bam_file.fetch():
+            index += 1
+            if index % 10_000:
+                print(index)
+    print("Total count ", index)          
+    return
+
+def open_bar_code_file_get_doublet_cells(barcode_file):
+    curr_donor = 0
+    prev_donor = 0
+    final_list_of_doublets = []
+    temp_list = []
+    with open(barcode_file, 'r') as file:
+        for line in file:
+            split_line = line.split("\t")
+            cell_id = split_line[0].split("-")[0]
+            curr_donor = int(split_line[0].split("-")[1])
+            if curr_donor == prev_donor:
+                temp_list.append(cell_id.strip())
+            else:
+                # process stuff, get 10 percent of cells and save it in final_list of doublets
+                percent_10_size = int(len(temp_list) / 18)
+                for value in random.sample(temp_list, percent_10_size):
+                    final_list_of_doublets.append("{}-{}".format(value, prev_donor))
+                    #print(value, curr_donor)
+                # clear the list
+                temp_list.clear()
+                temp_list.append(cell_id.strip())
+                prev_donor = curr_donor
+    print("Selected as doublet cells")
+    print(final_list_of_doublets)
+    return final_list_of_doublets
+
+def read_all_bam_files(doublet_list):
+    start_index = 1_000_000
+    end_index = 2_000_000
+    doublet_by_donor = [defaultdict(list) for _ in range(51)]
+    all_by_donor = [defaultdict(list) for _ in range(51)]
+    bam_file_path = "{}".format(INPUT_FILE_PATH)
+    index = 0
+    with pysam.AlignmentFile(bam_file_path, "rb") as bam_file:
+        # Iterate through all reads in the BAM file
+        for read in bam_file.fetch():
+            if read.has_tag("CB"):  # Check if read has "CB" tag
+                cb_tag = read.get_tag("CB")
+                donor_index = int(cb_tag.split("-")[1])
+                if donor_index < 51:
+                    if index < start_index:
+                        index += 1
+                        continue
+                    if cb_tag.strip() in doublet_list:
+                        #print("in doublet list", cb_tag)
+                        doublet_by_donor[donor_index][cb_tag].append(read)
+                    else:
+                        all_by_donor[donor_index][cb_tag].append(read)
+                    index += 1
+            if index % 1000 == 0:
+                print(index)
+                if index > end_index:
+                    break
+    return (all_by_donor, doublet_by_donor)
+
 
 def save_modified_reads(all_list, doublet_list, output_bam_path, output_barcodes_path, template_path):
     # Get the list of unique CB tags
@@ -73,7 +148,7 @@ def make_a_doublet_list(doublet_dics_by_donor):
         list_with_doublets.append((combined_donors_string, new_cb_tag, modified_reads))
         # Remove the cells from doublet_dics_by_donor
         del doublet_dics_by_donor[two_selected_donors[0]][cell_to_remove_1]
-        del doublet_dics_by_donor[two_selected_donors[1]][cell_to_remove_2] 
+        del doublet_dics_by_donor[two_selected_donors[1]][cell_to_remove_2]
         print("check len for delete ", len(doublet_dics_by_donor[two_selected_donors[0]]))
         print("check len for delete ", len(doublet_dics_by_donor[two_selected_donors[1]]))
         if len(doublet_dics_by_donor[two_selected_donors[0]]) == 0:
@@ -88,49 +163,6 @@ def make_a_doublet_list(doublet_dics_by_donor):
         if current_number_of_cells < 5 or len(doublet_dics_by_donor) < 2:
             doublet_dics_by_donor_empty = True
     return list_with_doublets
-
-def read_all_bam_files():
-    doublet_by_donor = [defaultdict(list) for _ in range(51)]
-    all_by_donor = [defaultdict(list) for _ in range(51)]
-    bam_file_path = "{}".format(INPUT_FILE_PATH)
-    index = 0
-    with pysam.AlignmentFile(bam_file_path, "rb") as bam_file:
-        # Iterate through all reads in the BAM file
-        for read in bam_file.fetch():
-            if read.has_tag("CB"):  # Check if read has "CB" tag
-                cb_tag = read.get_tag("CB")
-                donor_index = int(cb_tag.split("-")[1])
-                if donor_index < 51:
-                    all_by_donor[donor_index][cb_tag].append(read)
-                    index += 1
-            if index % 1000 == 0:
-                print(index)
-    print("retrieved all reads")
-    print("Pre removeal")
-    for index in range(51):
-        print(index, len(all_by_donor[index]))
-    # Go through each donor and select 1 percent from each
-    for donor_index in range(51):
-        # Select 10% of the barcodes for doublets
-        percent_1_size = int(len(all_by_donor[donor_index]) / 10)
-        print("1 percent doublet size", percent_1_size)
-        # Randomly select cells to remove
-        cells_to_remove = random.sample(list(all_by_donor[donor_index].keys()), percent_1_size)
-        print("cells to remove size ", len(cells_to_remove))
-        # Save the removed key-value pairs
-        temp_doublets = {cell: all_by_donor[donor_index][cell] for cell in cells_to_remove}
-        # Delete the entries from the original dictionary
-        for cell in cells_to_remove:
-            del all_by_donor[donor_index][cell]
-        # Create a dictionary for the sampled reads by CB tag
-        for key, value in temp_doublets.items():
-            # Append the value(s) for each key in the combined dictionary
-            # If the value is a list (e.g., reads associated with a CB tag), extend it
-            if isinstance(value, list):
-                doublet_by_donor[donor_index][key].extend(value)
-            else:
-                doublet_by_donor[donor_index][key].append(value)
-    return (all_by_donor, doublet_by_donor)
 
 if __name__ == "__main__":
     main()
