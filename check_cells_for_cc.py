929fbb73d31eb3e7bf035d12ca55b273cb6e79ad
minindu0 <mw8511@live.com>
2025-05-07 15:19:58 -0500

    update cell check

--- check_cells_for_cc.py
+++ check_cells_for_cc.py
@@ -13,31 +13,27 @@ def main():
 
 def process_file(file_path):
     clus_info = [[i, 0, 0.0, 0.0] for i in range(NUM_CLUS)]
+    total_cell_count = 0
     with open(file_path, 'r') as file:
         # Read all lines from the f
         for line in file:
+            total_cell_count += 1
             tabbed_line = line.split("\t")
             cell_id = tabbed_line[0]
             assigned_clus = int(tabbed_line[1])
             probs = tabbed_line[2:]
             prob_sum = 0
-            for prob in probs:
+            for index, prob in enumerate(probs):
                 prob_sum += float(prob)
             mean = prob_sum / NUM_CLUS
             assigned_prob = float(probs[assigned_clus])
             clus_info[assigned_clus][1] += 1
             clus_info[assigned_clus][2] += assigned_prob
-            clus_info[assigned_clus][3] += mean
+            clus_info[assigned_clus][3] += assigned_prob / mean
+    print("divided by cell count")
     for index, entry in enumerate(clus_info):
         print(index, entry)
-        clus_info[index][2] = clus_info[index][2] / (clus_info[index][1] + 0.000001)
-        clus_info[index][3] = clus_info[index][3] / (clus_info[index][1] + 0.000001)
     clus_info.sort(key=lambda x: x[1])
-    for index, entry in enumerate(clus_info):
-        print(index, entry)
-    clus_info.sort(key=lambda x: x[3])
-    for index, entry in enumerate(clus_info):
-        print(index, entry)
     return
 
 if __name__ == "__main__":
