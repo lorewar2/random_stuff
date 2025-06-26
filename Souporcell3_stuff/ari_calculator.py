7502afeb95152f1b173647c55eeb587e003024a9
minindu0 <mw8511@live.com>
2025-06-25 19:25:44 -0500

    update for vireo

--- Souporcell3_stuff/ari_calculator.py
+++ Souporcell3_stuff/ari_calculator.py
@@ -11,40 +11,6 @@ def main():
     file_path = sys.argv[1]  # Get file path from command-line argument
     process_file(file_path)
 
-# Function to process a pair of entries
-def process_pair(entry1, entry2):
-    a = False
-    b = False
-    c = False
-    d = False
-    index = 0
-    entry1_ground, entry1_calc = get_soupurcell_cluster_and_ground_truth(entry1)
-    entry2_ground, entry2_calc = get_soupurcell_cluster_and_ground_truth(entry2)
-    # check if both equal
-    if (entry1_ground == entry2_ground) and (entry1_calc == entry2_calc):
-        a = True
-        index = 0
-    if (entry1_ground != entry2_ground) and (entry1_calc != entry2_calc):
-        b = True
-        index = 1
-    if (entry1_ground == entry2_ground) and (entry1_calc != entry2_calc):
-        c = True
-        index = 2
-    if (entry1_ground != entry2_ground) and (entry1_calc == entry2_calc):
-        d = True
-        index = 3
-    assert((a + b + c + d) == 1) # something very wrong
-    return index
-
-def get_soupurcell_cluster_and_ground_truth(entry):
-    split_entry = entry.split("\t")
-    # get the ground truth cluster
-    ground_truth_cluster = int(split_entry[0].split("-")[1])
-    # get the soupercell cluster
-    soupurcell_cluster = int(split_entry[1])
-    #cluster_vectors = [float(value) for (value, index) in enumerate(split_entry) if index > 1]
-    return (ground_truth_cluster, soupurcell_cluster)
-
 # Read entries from a file
 def process_file(file_path):
     print("Going through lines")
@@ -56,14 +22,18 @@ def process_file(file_path):
         # Read all lines from the f
         for line in file:
             temp = line.strip().split("\t")[0].split("-")
-            if len(temp) == 2:
+            if len(temp) == 2 and line.strip().split("\t")[1][0:5] == "donor":
                 entries.append(line.strip())
                 # get the ground truth cluster
                 ground_truth_cluster = int(line.strip().split("\t")[0].split("-")[1])
                 cluster_ground.append(ground_truth_cluster)
             # get the soupercell cluster
-                soupurcell_cluster = int(line.strip().split("\t")[1])
+                soupurcell_cluster = int(line.strip().split("\t")[1][5:])
+                #soupurcell_cluster = int(line.strip().split("\t")[1])
                 cluster_method.append(soupurcell_cluster)
+            elif len(temp) == 2:
+                cluster_ground.append(0)
+                cluster_method.append(1)
             index += 1
             if index % 100 == 0:
                 print("Current line ", index)
